04ee6414af2990c3569a0cdcafb55bf022739082
Manuelcl <salvadordevlooper@gmail.com>
2022-11-08 11:11:58 -0600

    La lista de producto fue creada y añadida desde Javascript

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+// Navbar - Menu
+
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartIconMenu = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,20 +14,19 @@ burgerMenu.addEventListener('click', toggleBurgerMenu);
 cartIconMenu.addEventListener('click', toggleCartAside);
 
 
-const isAsideClosed = aside.classList.contains('inactive');
-const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
-
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
     desktopMenu.classList.toggle('inactive');
 
-    if(isAsideClosed) {
+    if(!isAsideClosed) {
         aside.classList.add('inactive');
     }
 }
 
 function toggleBurgerMenu() {
-    if(isAsideClosed) {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
         aside.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
@@ -33,12 +35,69 @@ function toggleBurgerMenu() {
 function toggleCartAside() {
     // const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
     // const isAsideOpen = !aside.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
 
-    if(isMobileMenuClosed) {
+    if(!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
-    if(isDesktopMenuClose) {
+    if(!isDesktopMenuClose) {
         desktopMenu.classList.add('inactive');
     }
     aside.classList.toggle('inactive');
 }
+
+// Product list
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""',
+
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""'
+});
+productList.push({
+    name: 'Computadora',
+    price: 600,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""'
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        const productName = document.createElement('p');
+    
+        productPrice.innerText = '$' + product.price;
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productIconCart = document.createElement('img');
+        productIconCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productIconCart);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
